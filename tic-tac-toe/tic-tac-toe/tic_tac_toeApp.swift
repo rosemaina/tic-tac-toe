82f648752ec1e7b18c02beca70d1ada50935a799
Rose Maina <rmaina3@safaricom.co.ke>
2023-12-19 10:58:50 +0300

    Add swift project folder

---
+++ tic-tac-toe/tic-tac-toe/tic_tac_toeApp.swift
@@ -0,0 +1,17 @@
+//
+//  tic_tac_toeApp.swift
+//  tic-tac-toe
+//
+//  Created by Rose Maina on 19/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct tic_tac_toeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
